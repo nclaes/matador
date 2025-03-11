eaf1b5f4e3f9e3a79bfb9c8f9e12dde9691cbe21
tousifrahman <tousifrahman@gmail.com>
2025-03-11 09:25:34 +0000

    testbench added

--- MATADOR_main.py
+++ MATADOR_main.py
@@ -3,11 +3,203 @@ import os
 import numpy as np
 from math import ceil 
 from math import log
+import math
 from utils.train import prep_data, checkconfig
 from utils.tmu.models.classification.vanilla_classifier import TMClassifier
 from utils.tmu.models.classification.coalesced_classifier import TMCoalescedClassifier
 from utils.tmu.tools import BenchmarkTimer
 # from utils.rtl import coalesced_tm_write_hard_coded_blocks, coalesced_tm_hard_coded_blocks_top
+
+def write_testbench(tb_file, AXI_data_width, number_of_blocks):
+    # testing for 10 data points
+	with open(tb_file, "w") as f:
+		print("module axis_stream_tb();", file=f)
+		print("\t\tparameter C_S00_AXIS_DATA_WIDTH = %d;"% AXI_data_width, file=f)
+		print("\t\tparameter C_M00_AXIS_DATA_WIDTH = %d;"% AXI_data_width, file=f)
+		print("\t\tparameter NUM_PACKETS = %d;"% int(number_of_blocks), file=f)
+		print("\t\tparameter DATAPOINTS = %d;" % 10, file=f)
+		print("""
+
+    reg clock;
+    reg areset;
+    
+    reg s00_axis_tready;
+    reg m00_axis_tready;
+    reg s00_last;
+    reg m00_last; 
+    reg m00_valid; 
+    reg s00_valid; 
+    reg [C_S00_AXIS_DATA_WIDTH-1:0] payload; 
+    reg [C_M00_AXIS_DATA_WIDTH-1:0] recieved;
+    reg [63:0] input_data [0:DATAPOINTS * NUM_PACKETS - 1]; 
+    
+    wire [7:0] bytes; 
+    int input_counter = 0; 
+    
+    reg last_triggered; 
+    
+    axis_wrapper_top  
+        DUT(
+            .s00_axis_aclk(clock),
+            .s00_axis_aresetn(areset),
+            .s00_axis_tready(s00_axis_tready),                  
+            .s00_axis_tdata(payload),
+            .s00_axis_tstrb(bytes),
+            .s00_axis_tlast(s00_last),
+            .s00_axis_tvalid(s00_valid),
+            .m00_axis_aclk(clock),
+            .m00_axis_aresetn(areset),
+            .m00_axis_tready(m00_axis_tready),
+            .m00_axis_tdata(recieved),                                                     
+            .m00_axis_tlast(m00_last),                          
+            .m00_axis_tvalid(m00_valid)                        
+        );
+ 
+    initial begin    
+        last_triggered = 1'b0;                                          
+        #0 clock = 0;
+        forever begin
+            #20 clock = ~clock;
+        end 
+    end
+    initial begin     
+        $readmemb("test_data_copy_paste_examples.mem", input_data);
+        areset <= 1'b1;
+        m00_axis_tready     <= 1'b1;
+        #20 s00_last        <= 1'b0;
+        #20 areset          <= 1'b0;
+        #20 areset          <= 1'b1;
+        #20 s00_valid       <= 1'b0;
+        
+        #20 payload         <= input_data[0];
+        s00_valid           <= 1'b1;
+    end
+    integer cnt = 0;
+    integer stop_cnt = 0;
+    always @(posedge clock) begin  
+        if(s00_axis_tready && s00_valid) begin
+            if(input_counter == DATAPOINTS*NUM_PACKETS-2) begin 
+               s00_last     <= 1'b1;  
+            end
+			// Note you may have to change this line here 
+            else if (input_counter == 160) begin
+                s00_valid = 0;
+            end
+            
+            if(s00_last == 1'b1) begin 
+                s00_last    <= 1'b0;
+                s00_valid   <= 1'b0;
+            end
+            input_counter   <= input_counter + 1;
+            payload         = input_data[input_counter];
+        end 
+        else if (!s00_valid) begin
+            if (cnt < NUM_PACKETS-1) begin
+                cnt = cnt + 1;
+            end
+            else if (cnt == NUM_PACKETS-1) begin
+//                s00_valid = 1;
+                cnt = 0;
+            end
+        end
+        if (input_counter == 4) begin
+            m00_axis_tready     <= 1'b0;
+        end
+        if (m00_axis_tready == 0) begin
+            stop_cnt = stop_cnt + 1;
+            if (stop_cnt == 6) begin
+                stop_cnt = 0;
+                m00_axis_tready     <= 1'b1;
+            end
+        end
+        if(m00_last) begin 
+            last_triggered = 1'b1;
+        end 
+        else if(last_triggered) begin 
+            input_counter = 0; 
+            s00_last    <= 1'b0;
+            s00_valid   <= 1'b1;
+            last_triggered = 1'b0;
+            payload         <= input_data[0];
+        end
+    end
+
+endmodule
+	
+	""", file=f)
+          
+
+def pack_data(test_data, output_dir, AXI_data_width):
+
+	mnist_file = test_data
+	File_data = np.loadtxt(mnist_file, dtype=int)
+	number_of_examples = 10
+	# Remove the class cols 
+	cols = File_data[:, -1]
+
+	f = open(output_dir + "/RTL/expected_answers.txt", "w")
+	for i in range(number_of_examples):
+		f.write(str(cols[i])+"\n")
+	f.close()
+
+	File_data = File_data[:, :-1]
+	AXI_bus_size =  AXI_data_width
+
+	print("	[RTL_gen][d] Number of features:\t\t", File_data[0].shape[0])
+	print("	[RTL_gen][d] Packets:\t\t\t", File_data[0].shape[0]/AXI_bus_size)
+	print("	[RTL_gen][d] Number of Extra packet(s):\t", math.ceil(File_data[0].shape[0]/AXI_bus_size) - math.floor(File_data[0].shape[0]/AXI_bus_size))
+	print("	[RTL_gen][d] Number of packets required:\t", math.ceil(File_data[0].shape[0]/AXI_bus_size))
+
+	mnist_packets = []
+	packet_32 = []
+	packet_counter = 0 
+
+	for j in range(number_of_examples):
+		for i in range(File_data[j].shape[0]):
+			if packet_counter <= AXI_bus_size-1: 
+				packet_32.append(File_data[j][i])
+			else: 
+				mnist_packets.append(packet_32)
+				packet_32 = []
+				# print(i)
+				packet_counter = 0
+				packet_32.append(File_data[j][i])
+
+			packet_counter += 1
+
+		# If datapoint is complete and packet is not fully filled...
+		# Fill the remainder with zeros 
+		if(len(packet_32) != 0):
+			# print("Half filled packet: ", len(packet_32))
+			remainder_zero_fill = AXI_bus_size -len(packet_32)   
+			for l in range(remainder_zero_fill):
+				packet_32.append(0)
+			
+			mnist_packets.append(packet_32)
+			packet_counter = 0
+			packet_32 = []
+	# convert the list of lists to a numpy array 
+	mnist_packets_np_1 = np.array(mnist_packets)
+	mnist_packets_np  = np.fliplr(mnist_packets_np_1)
+	# print(mnist_packets_np)
+	# now convert each 32 number array to its equivalent binary 
+	# this will be the 32 bit packet sent each time 
+	mnist_packets_np_bits =np.packbits(mnist_packets_np_1, axis=-1,  bitorder='little')
+	if(AXI_bus_size > 64):
+		print("	[RTL_gen]	Bus size is greater than maximum")
+	else:	
+		mnist_packets_np_bits.dtype = np.uint64
+	# print(mnist_packets_np_bits)
+	# now the mnist data is in 32 bit packets -- we can write the testbench 
+	
+	tb_file_test = output_dir + "/RTL/test_data_copy_paste_examples.mem"
+	f = open(tb_file_test, "w")
+
+	for i in range(mnist_packets_np.shape[0]):
+		# if i %13 == 0:
+			# print("------")
+		# print(mnist_packets_np_bits[i])
+		print(''.join(map(str, mnist_packets_np[i])), file=f)
 
 def to_bin(val, bits):
     s = bin(val & int("1"*bits, 2))[2:]
@@ -728,8 +920,13 @@ def generate_rtl(config):
 
         os.system("cp utils/RTL_templates/AXI_Interface.sv "+ output_directory+"/RTL")
         print("	[RTL_gen]	AXI_Interface.sv \tis added")
-
-
+        print("    ----------------------------------------")
+        print("            Testbench generation            ")
+        print("    ----------------------------------------")
+        tb  = output_directory + "/RTL/testbench.sv"
+        pack_data(test_data, output_directory, bus_width)
+        write_testbench(tb, bus_width, number_of_blocks)
+		
 
 
 def synthesize_and_implement(config):
